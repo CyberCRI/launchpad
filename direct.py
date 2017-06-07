85d6f564f6cddafef6e62d6a19776c41d576e55c
Jesse Himmelstein <jesse@heavenstone.net>
2017-06-07 15:19:28 +0200

    Showing position of touch

--- direct.py
+++ direct.py
@@ -6,6 +6,8 @@ import collections
 import copy
 
 
+#### Utility functions ####
+
 Position = collections.namedtuple('Position', ['x', 'y'])
 
 def pos_to_led(pos):
@@ -22,10 +24,7 @@ def are_4_adjacent(pos_a, pos_b):
 
 Drop = collections.namedtuple('Drop', ['position', 'color'])
 
-drops = [Drop(Position(0, 0), 'red')]
-
 color_name_to_code = { "red": 5, "green": 32, "blue": 48, "white": 3 }
-
 
 # type is 'solid', 'pulse', or 'blink'
 DrawCommand = collections.namedtuple('DrawCommand', ['type', 'color'])
@@ -50,6 +49,26 @@ def execute_draw_command(pos, command):
     else:
         raise ValueError('command has unrecognized type: "%s"' % (command.type))
 
+old_draw_buffer = make_empty_draw_buffer()
+def draw(new_draw_buffer):
+    global old_draw_buffer
+
+    for y in range(8):
+        for x in range(8):
+            if new_draw_buffer[y][x] != old_draw_buffer[y][x]:
+                execute_draw_command(Position(x, y), new_draw_buffer[y][x])
+
+    old_draw_buffer = copy.deepcopy(new_draw_buffer)
+
+
+#### Global game data
+
+drops = [Drop(Position(0, 0), 'red')]
+next_positions = set()
+
+
+#### Game Logic ####
+
 
 def init():
     launchpad_util.connect()
@@ -69,34 +88,27 @@ def cleanup():
 def on_touch(message):
     did_touch = (message[0][2] == 127)
     led = message[0][1]    
+    touch_pos = led_to_pos(led)
     if did_touch:
         # print "Touched LED %s" % (led)
-        touch_pos = led_to_pos(led)
         if are_4_adjacent(drops[0].position, touch_pos):
-            drops[0] = Drop(touch_pos, drops[0].color)
+            next_positions.add(touch_pos)
     else:
         # print "Released LED %s" % (led)
-        #launchpad_util.clear_led(led)
-        pass
+        if are_4_adjacent(drops[0].position, touch_pos):
+            next_positions.remove(touch_pos)
+            drops[0] = Drop(touch_pos, drops[0].color)
 
 def make_draw_buffer():
     draw_buffer = make_empty_draw_buffer()
 
     for drop in drops:
-        draw_buffer[drop.position.y][drop.position.x] = DrawCommand('solid', drop.color)
+        draw_buffer[drop.position.y][drop.position.x] = DrawCommand('pulse', drop.color)
+
+    for next_pos in next_positions:
+        draw_buffer[next_pos.y][next_pos.x] = DrawCommand('solid', 'white')
 
     return draw_buffer
-
-old_draw_buffer = make_empty_draw_buffer()
-def draw(new_draw_buffer):
-    global old_draw_buffer
-
-    for y in range(8):
-        for x in range(8):
-            if new_draw_buffer[y][x] != old_draw_buffer[y][x]:
-                execute_draw_command(Position(x, y), new_draw_buffer[y][x])
-
-    old_draw_buffer = copy.deepcopy(new_draw_buffer)
 
 def main_loop():
     while 1:
