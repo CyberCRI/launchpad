848b8d42cf23deeaa5fb25acab3f088e43688407
Jesse Himmelstein <jesse@heavenstone.net>
2017-06-07 18:11:23 +0200

    Map reading and obstacles

--- direct.py
+++ direct.py
@@ -22,8 +22,6 @@ def are_4_adjacent(pos_a, pos_b):
     return diff_x == 1 and diff_y == 0 or diff_x == 0 and diff_y == 1 
 
 
-Drop = collections.namedtuple('Drop', ['position', 'color'])
-
 color_name_to_code = {
     'red': 5, 
     'yellow': 13, 
@@ -31,7 +29,9 @@ color_name_to_code = {
     'green': 32, 
     'orange': 9, 
     'purple': 48,
-    "white": 3 
+    'white': 3,
+    'pink': 57,
+    'brown': 61
 }
 
 # Tuples made in alphabetical order
@@ -45,7 +45,13 @@ def mix_colors(a, b):
     if a == b: return a
 
     color_combo = tuple(sorted([a, b]))
-    return color_mixing_map[color_combo]
+    if color_combo in color_mixing_map:
+        return color_mixing_map[color_combo]
+    else:
+        return 'brown'
+
+
+Drop = collections.namedtuple('Drop', ['position', 'color'])
 
 
 # type is 'solid', 'pulse', or 'blink'
@@ -85,12 +91,39 @@ def draw(new_draw_buffer):
 
 #### Global game data
 
+# These have default values in case no map is given
 drops = [Drop(Position(0, 0), 'red'), Drop(Position(7, 0), 'blue')]
+obstacles = {Position(7, 3), Position(6, 3), Position(5, 3)}
+
 next_positions = set()
 pressed_leds_count = 0
 
 
 #### Game Logic ####
+
+def read_map(map_str):
+    global drops, obstacles
+
+    drops = []
+    obstacles = set()
+
+    lines = map_str.split('\n')
+    y = 0
+    for line in lines:
+        # If line is empty or starts with '#'' skip it
+        if (not line) or line[0] == '#': continue
+
+        for x, c in enumerate(line):
+            if c == 'r': 
+                drops.append(Drop(Position(x,y), 'red'))
+            elif c == 'b': 
+                drops.append(Drop(Position(x,y), 'blue'))
+            elif c == 'y': 
+                drops.append(Drop(Position(x,y), 'yellow'))
+            elif c == 'x':
+                obstacles.add(Position(x,y))
+
+        y += 1
 
 def init():
     launchpad_util.connect()
@@ -116,9 +149,7 @@ def find_adjacent_drop_indices(pos):
     return indices
 
 def on_touch(message):
-    global next_positions
-    global drops
-    global pressed_leds_count
+    global next_positions, drops, pressed_leds_count
 
     did_touch = (message[0][2] == 127)
     led = message[0][1]    
@@ -127,7 +158,7 @@ def on_touch(message):
         # print "Touched LED %s" % (led)
         pressed_leds_count += 1
 
-        if find_adjacent_drop_indices(touch_pos):
+        if (touch_pos not in obstacles) and find_adjacent_drop_indices(touch_pos):
             next_positions.add(touch_pos)
     else:
         # print "Released LED %s" % (led)
@@ -177,6 +208,9 @@ def make_draw_buffer():
     for next_pos in next_positions:
         draw_buffer[next_pos.y][next_pos.x] = DrawCommand('solid', 'white')
 
+    for obstacle in obstacles:
+        draw_buffer[obstacle.y][obstacle.x] = DrawCommand('solid', 'pink')
+
     return draw_buffer
 
 def main_loop():
@@ -193,6 +227,11 @@ def main_loop():
 
 if __name__ == '__main__':
     try:
+        if len(sys.argv) > 1:
+            print("Reading map from file '%s'" % (sys.argv[1]))
+            map_file = open(sys.argv[1])
+            read_map(map_file.read())
+
         init()
         main_loop()
     except KeyboardInterrupt:
